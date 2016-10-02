d6a59d2c1a82cf30ff702aeea45bb9fea32fb76f
polypmer <exorable.ludos@gmail.com>
2016-10-02 12:42:33 -0400

    Added local address/network retrieval

---
+++ local.go
@@ -0,0 +1,36 @@
+package peekaboo
+
+import (
+	"errors"
+	"net"
+)
+
+func LocalAddress() (net.IP, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP, nil
+			}
+		}
+	}
+	return nil, errors.New("No IP found")
+}
+
+func LocalNetwork() (*net.IPNet, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return nil, err
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			return ipnet, nil
+		}
+	}
+	return nil, errors.New("No Network Found")
+}
--- peek/peek.go
+++ peek/peek.go
@@ -1,13 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-//"github.com/polypmer/peekaboo"
+	"github.com/polypmer/peekaboo"
+)
 
 func main() {
-	fmt.Println("Pinging Hosts in CIDR")
+	//fmt.Println("Pinging Hosts in CIDR")
 	//alives, _ := peekaboo.AliveHosts("192.168.1.0/24")
 
 	//fmt.Println(alives)
+	a, _ := peekaboo.LocalAddress()
+	fmt.Println(a)
+	n, _ := peekaboo.LocalNetwork()
+	fmt.Println(n)
 
 }
