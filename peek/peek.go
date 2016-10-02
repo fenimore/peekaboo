d00cc451093da0b2196a463f8a463740265c87f2
polypmer <exorable.ludos@gmail.com>
2016-10-02 13:19:49 -0400

    Change timeout to be feasible. Also why isnt tor showing up?

--- peek/peek.go
+++ peek/peek.go
@@ -1,22 +1,21 @@
 package main
 
-import "github.com/polypmer/peekaboo"
+import (
+	"fmt"
+
+	"github.com/polypmer/peekaboo"
+)
 
 func main() {
-	//fmt.Println("Pinging Hosts in CIDR")
-	//alives, _ := peekaboo.AliveHosts("192.168.1.0/24")
-
-	//fmt.Println(alives)
-	//a, _ := peekaboo.LocalAddress()
-	//fmt.Println(a)
-	n, _ := peekaboo.LocalNetwork()
-	//fmt.Println(n)
-
+	//n, _ := peekaboo.LocalNetwork()
+	// fmt.Println("Scanning for alives on: ", n)
 	// TODO: does this work with the IPNet string?
 	// insteald of 0/24 it's something like 144/24
-	alives, _ := peekaboo.AliveHosts(n.String())
+	// It does send pings to the right hosts,
+	// but doesn't pick up on all of them?
+	alives, _ := peekaboo.AliveHosts("192.168.1.0/24") //n.String())
 
-	//fmt.Println(alives)
+	fmt.Println("Found: ", alives)
 
 	peekaboo.PortScan(alives)
 }
--- scanner.go
+++ scanner.go
@@ -10,10 +10,10 @@ import (
 func PortScan(ips []string) {
 
 	for _, ip := range ips {
-
+		fmt.Println("scanning ", ip)
 		// Scanner
-		ps := portscanner.NewPortScanner(ip, 20*time.Second)
-		openedPorts := ps.GetOpenedPort(20, 30000)
+		ps := portscanner.NewPortScanner(ip, time.Second)
+		openedPorts := ps.GetOpenedPort(20, 10000)
 		//fmt.Println(openedPorts)
 		for i := 0; i < len(openedPorts); i++ {
 			port := openedPorts[i]
